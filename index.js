a0f9541ba0dab938e4f550071e4627b048582940
Kevin Wang <139580194+kswang06@users.noreply.github.com>
2026-04-14 14:28:37 -0400

    fix vercel

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ import UserRoutes from "./kambaz/users/routes.js";
 import CourseRoutes from "./kambaz/courses/routes.js";
 import "dotenv/config";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import ModulesRoutes from "./kambaz/modules/routes.js";
 import AssignmentRoutes from "./kambaz/assignments/routes.js";
 import EnrollmentsRoutes from "./kambaz/enrollments/routes.js";
 import QuizzesRoutes from "./kambaz/quizzes/routes.js";
 
-const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+const CONNECTION_STRING =
+  process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
-const app = express()
+
+const app = express();
 const isProduction =
   process.env.NODE_ENV === "production" ||
   process.env.SERVER_ENV === "production";
@@ -37,28 +40,37 @@ app.use(
     credentials: true,
   })
 );
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: CONNECTION_STRING,
+    collectionName: "sessions",
+  }),
 };
+
 if (isProduction) {
   app.set("trust proxy", 1);
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   };
 }
-app.use(session(sessionOptions));
 
-app.use(express.json());   
+app.use(session(sessionOptions));
+app.use(express.json());
+
 UserRoutes(app, db);
 CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentRoutes(app, db);
 EnrollmentsRoutes(app);
 QuizzesRoutes(app);
-Lab5(app) 
-Hello(app)
-app.listen(process.env.PORT || 4000)
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
